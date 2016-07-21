fbc66e949f87b914e66325f4616245f9e9fcb9be
Mauricio Muriel <mauricio.muriel@calitek.net>
2016-07-21 01:08:00 -0500

    Adicionando los gradientes estaticos en el fondo via CSS

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+'use strict';
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+ 
+
+gulp.task('hello',function(){
+
+	let date = new Date();
+	console.log(`Hola mundo a las ${date.toUTCString()}`);
+
+
+});
+
+
+gulp.task('initwatch',function(){
+
+	browserSync.init({
+		server: {
+		  baseDir: 'app'
+		},
+	});
+
+
+	gulp.watch('app/*.html').on("change",browserSync.reload);
+	gulp.watch('app/css/*.css').on("change",browserSync.reload);
+
+});
+
+gulp.task('init',['initwatch','hello']);
+
+
+
+
